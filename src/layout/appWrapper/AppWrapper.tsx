87da021491d7af01e6de1cead644fac8e346d87a
Austin Howard <cnfishead@hotmail.com>
2025-06-30 13:53:35 -0400

    continuing setup, removal of unused items

--- src/layout/appWrapper/AppWrapper.tsx
+++ src/layout/appWrapper/AppWrapper.tsx
@@ -1,15 +1,11 @@
-'use client';
-import { useQueryClient } from '@tanstack/react-query';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
-import io from 'socket.io-client';
-import { useUser } from '@/state/auth';
-import { useSocketStore } from '@/state/socket';
-import PageLayout from '../page/Page.layout';
-import BillingSetup from '../billingSetup/BillingSetup.layout';
-import { navigation } from '@/data/navigation';
-import useApiHook from '@/hooks/useApi';
-import { Skeleton } from 'antd';
+"use client";
+import { useQueryClient } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
+import { useUser } from "@/state/auth";
+import { useSocketStore } from "@/state/socket";
+import useApiHook from "@/hooks/useApi";
 
 type Props = {
   children: React.ReactNode;
@@ -18,13 +14,13 @@ const AppWrapper = (props: Props) => {
   const queryClient = useQueryClient();
   //Set up state
   const searchParams = useSearchParams();
-  const token = searchParams.get('token') as string;
+  const token = searchParams.get("token") as string;
   const { data: loggedInData, isLoading: userIsLoading } = useUser(token);
   const { data: selectedProfile } = useApiHook({
-    method: 'GET',
-    key: ['profile', 'athlete'],
-    url: `/athlete/profile/${loggedInData?.profileRefs['athlete']}`,
-    enabled: !!loggedInData?.profileRefs['athlete'],
+    method: "GET",
+    key: ["profile", "admin"],
+    url: `/admin/profile/${loggedInData?.profileRefs["admin"]}`,
+    enabled: !!loggedInData?.profileRefs["admin"],
   });
   //Set up socket connection
   const { socket, isConnecting, setSocket, setIsConnecting } = useSocketStore((state: any) => state);
@@ -32,8 +28,10 @@ const AppWrapper = (props: Props) => {
   useEffect(() => {
     if (process.env.API_URL) {
       setIsConnecting(true);
-      const socket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.API_URL.replace('/api/v1', ''));
-      socket.on('connect', () => {
+      const socket = io(
+        process.env.NODE_ENV === "development" ? "http://localhost:5000" : process.env.API_URL.replace("/api/v1", "")
+      );
+      socket.on("connect", () => {
         setIsConnecting(false);
         setSocket(socket);
       });
@@ -48,10 +46,17 @@ const AppWrapper = (props: Props) => {
 
     if (socket && isConnecting) {
       // Listen for user updates
-      socket.emit('setup', loggedInData);
-      socket.on('updateUser', () => {
-        queryClient.invalidateQueries(['user'] as any);
+      socket.emit("setup", loggedInData);
+      socket.on("updateUser", () => {
+        queryClient.invalidateQueries(["user"] as any);
       });
+    }
+
+    // if selectedProfile is null or role is not admin, dispatch the token, logout and redirect
+    // alert user that they are not authorized to access the admin portal
+    if (!selectedProfile || selectedProfile?.payload?.role !== "admin") {
+      alert("You are not authorized to access this portal.");
+      // Dispatch logout action and redirect
     }
 
     return () => {
@@ -59,19 +64,9 @@ const AppWrapper = (props: Props) => {
         socket.close();
       }
     };
-  }, [socket]);
- 
-  return (
-    <>
-      {selectedProfile?.payload?.needsBillingSetup ? (
-        <PageLayout pages={[navigation().billing.links.account_center]} loading={userIsLoading || !selectedProfile}>
-          <BillingSetup />
-        </PageLayout>
-      ) : (
-        <>{props.children}</>
-      )}
-    </>
-  );
+  }, [socket, selectedProfile]);
+
+  return <>{props.children}</>;
 };
 
 export default AppWrapper;
